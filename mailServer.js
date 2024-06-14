79cdfb3d9107e9348e464eec30d73dec6dd958b7
Hashara13 <hnethmin13@gmail.com>
2024-06-14 09:24:52 +0530

    updated mail server

---
+++ mailServer.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
+const app = express();
+app.use(bodyParser.json());
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS 
+  }
+});
+
+app.post('/send-email', (req, res) => {
+  const { to, subject, text } = req.body;
+
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to,
+    subject,
+    text
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return res.status(500).send(error.toString());
+    }
+    res.send('Email sent to : ' + info.response);
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is running succesfully on port 3000');
+});
